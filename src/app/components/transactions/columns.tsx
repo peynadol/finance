8c16f4ef1cff410880e815ee7f11b44d95f0163a
peynadol <peynadol@gmail.com>
2025-06-29 18:14:31 +0100

    style table content

--- src/app/components/transactions/columns.tsx
+++ src/app/components/transactions/columns.tsx
@@ -20,30 +20,68 @@ export const columns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "name",
     header: () => (
-      <div className="text-left text-preset-5 text-grey-900">Name</div>
+      <div className="text-left text-preset-5 text-grey-500">
+        Recipient / Sender
+      </div>
     ),
+    cell: ({ row }) => {
+      const name: string = row.getValue("name");
+      const avatar: string = row.getValue("avatar");
+      return (
+        <div className="flex items-center gap-2">
+          <Image
+            src={avatar}
+            alt={name}
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+          <span className="text-preset-4-bold text-grey-900">{name}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "category",
     header: () => (
-      <div className="text-left text-preset-5 text-grey-900">Category</div>
+      <div className="text-left text-preset-5 text-grey-500">Category</div>
     ),
+    cell: ({ row }) => {
+      const category: string = row.getValue("category");
+      return (
+        <div className="text-preset-5 text-grey-500">
+          {category.charAt(0).toUpperCase() + category.slice(1)}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "date",
     header: () => (
-      <div className="text-left text-preset-5 text-grey-900">Date</div>
+      <div className="text-left text-preset-5 text-grey-500">Date</div>
     ),
+    cell: ({ row }) => {
+      const date: string = row.getValue("date");
+      return (
+        <div className="text-preset-5 text-grey-500">
+          {format(new Date(date), "dd MMM yyyy")}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "amount",
     header: () => (
-      <div className="text-right text-preset-5 text-grey-900">Amount</div>
+      <div className="text-right text-preset-5 text-grey-500">Amount</div>
     ),
     cell: ({ row }) => {
       const amount: number = row.getValue("amount");
       return (
-        <div className="text-right text-preset-5 text-grey-900">
+        <div
+          className={`text-right text-preset-4-bold ${
+            amount > 0 ? "text-green" : "text-grey-900"
+          }`}
+        >
           {amount > 0
             ? `+£${amount.toFixed(2)}`
             : `-£${Math.abs(amount).toFixed(2)}`}
