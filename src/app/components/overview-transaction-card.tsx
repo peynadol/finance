c8a15bd89936aabf45f1813007f9f0f67d369c35
peynadol <peynadol@gmail.com>
2025-06-30 16:01:43 +0100

    style and pass data into transactions overview container

--- src/app/components/overview-transaction-card.tsx
+++ src/app/components/overview-transaction-card.tsx
@@ -1,25 +1,54 @@
 //TODO: in future, have the colour of the amount change based on whether its income or outgoing
-//TODO: add avatar images for the transactions
+//TODO: add avatar images for the transactions{" "}
+import { format } from "date-fns";
+import clsx from "clsx";
+import Image from "next/image";
 type OverviewTransactionCardProps = {
   name: string;
   amount: number;
   date: string;
+  avatarPath?: string; // Optional avatar URL
 };
 const OverviewTransactionCard = ({
   name,
   amount,
   date,
+  avatarPath = "", // Default to empty string if no avatar URL is provided
 }: OverviewTransactionCardProps) => {
+  const formattedDate = format(new Date(date), "dd MMM yyyy");
+  const isPositive = amount >= 0;
+  const formattedAmount = `${isPositive ? "+" : "-"}£${Math.abs(amount).toFixed(
+    2
+  )}`;
+
   return (
     <div className="flex gap-4 bg-transparent py-4 w-full border-t border-grey-100 justify-between ">
       <div className="flex items-center gap-4">
-        <div className="rounded-full bg-red h-10 w-10"></div>{" "}
-        {/* Placeholder for avatar */}
+        <div className="">
+          {avatarPath ? (
+            <Image
+              src={avatarPath}
+              alt={name}
+              className="rounded-full h-full w-full"
+              width={40}
+              height={40}
+            />
+          ) : (
+            <div className="bg-grey-200 h-full w-full rounded-full"></div>
+          )}
+        </div>
         <p className="text-preset-4-bold">{name}</p>
       </div>
       <div>
-        <p className="text-preset-4-bold text-green">+£{amount}</p>
-        <p className="text-preset-5">{date}</p>
+        <p
+          className={clsx(
+            "text-preset-4-bold text-right",
+            isPositive ? "text-green" : "text-grey-900"
+          )}
+        >
+          {formattedAmount}
+        </p>
+        <p className="text-preset-5">{formattedDate}</p>
       </div>
     </div>
   );
--- src/app/components/overview-transaction-container.tsx
+++ src/app/components/overview-transaction-container.tsx
@@ -1,7 +1,10 @@
 import { ChevronRight } from "lucide-react";
 import OverviewTransactionCard from "./overview-transaction-card";
 import Link from "next/link";
+import { useStore } from "@/lib/useStore";
 const OverviewTransactionContainer = () => {
+  const transactions = useStore((state) => state.transactions);
+  console.log("Transactions:", transactions);
   return (
     <div className="bg-white rounded-lg p-8 ">
       <div className="flex justify-between items-center mb-4">
@@ -15,31 +18,15 @@ const OverviewTransactionContainer = () => {
       </div>
 
       <div className="flex items-center  flex-col gap-4">
-        <OverviewTransactionCard
-          name="Emma Richardson"
-          amount={100}
-          date="19 Aug 2024"
-        />
-        <OverviewTransactionCard
-          name="Emma Richardson"
-          amount={100}
-          date="19 Aug 2024"
-        />
-        <OverviewTransactionCard
-          name="Emma Richardson"
-          amount={100}
-          date="19 Aug 2024"
-        />
-        <OverviewTransactionCard
-          name="Emma Richardson"
-          amount={100}
-          date="19 Aug 2024"
-        />
-        <OverviewTransactionCard
-          name="Emma Richardson"
-          amount={100}
-          date="19 Aug 2024"
-        />
+        {transactions.slice(0, 5).map((transaction, index) => (
+          <OverviewTransactionCard
+            key={index} // not ideal, but okay for now
+            name={transaction.name}
+            amount={transaction.amount}
+            date={transaction.date}
+            avatarPath={transaction.avatar.replace(/^\.\/assets/, "")}
+          />
+        ))}
       </div>
     </div>
   );
