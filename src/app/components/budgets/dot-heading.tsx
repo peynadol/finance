2598bd55c2de6e49645ca2e0a9fb479ba42d5671
peynadol <peynadol@gmail.com>
2025-07-02 20:46:24 +0100

    add ellipsis dropdown menu to DotHeader component

--- src/app/components/budgets/dot-heading.tsx
+++ src/app/components/budgets/dot-heading.tsx
@@ -1,3 +1,9 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Ellipsis } from "lucide-react";
 //TODO: hook ellipsis up to a dropdown menu
 const DotHeading = ({ label, colour }: { label: string; colour: string }) => (
@@ -9,9 +15,25 @@ const DotHeading = ({ label, colour }: { label: string; colour: string }) => (
       />
       <h2 className="text-preset-2 text-grey-900">{label}</h2>
     </div>
-    <div>
-      <Ellipsis className="text-grey-500" />
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className="p-1 rounded-full hover:bg-muted focus:outline-none focus-visible:ring-2 focus-visible:ring-ring cursor-pointer"
+          aria-label="Open options"
+        >
+          <Ellipsis className="text-grey-500" />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => console.log("Edit")}>
+          Edit
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => console.log("Delete")}>
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </div>
 );
 
