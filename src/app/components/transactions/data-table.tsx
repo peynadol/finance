9e03dc30cc43fd0639d500108f8e70f734bfa564
peynadol <peynadol@gmail.com>
2025-07-08 10:49:01 +0100

    add transaction table search/filter for mobile

--- src/app/components/transactions/data-table.tsx
+++ src/app/components/transactions/data-table.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Funnel, ArrowDownUp } from "lucide-react";
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -69,22 +70,103 @@ export function DataTable<TData extends { category: string }, TValue>({
 
   return (
     <div>
-      <div className="flex items-center py-4 justify-between">
-        <div>
+      {/* Mobile filter/search bar */}
+      <div className="block md:hidden w-full mb-4">
+        <div className="flex items-center gap-2 bg-[#F9F6F3] p-2 rounded-xl">
+          {/* Search Input */}
           <Input
-            placeholder="Search recipient or sender..."
+            placeholder="Search transaction"
             value={
               (table.getColumn("recipient")?.getFilterValue() as string) ?? ""
             }
             onChange={(event) =>
               table.getColumn("recipient")?.setFilterValue(event.target.value)
             }
-            className="max-w-sm"
+            className="flex-1 bg-white text-sm"
           />
-        </div>
 
-        {/* Category Filter */}
+          {/* Sort Icon */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <ArrowDownUp className="w-5 h-5 text-grey-700" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem
+                onSelect={() => table.setSorting([{ id: "date", desc: true }])}
+              >
+                Latest
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() => table.setSorting([{ id: "date", desc: false }])}
+              >
+                Oldest
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() =>
+                  table.setSorting([{ id: "amount", desc: true }])
+                }
+              >
+                Highest
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() =>
+                  table.setSorting([{ id: "amount", desc: false }])
+                }
+              >
+                Lowest
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* Filter Icon */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <Funnel className="w-5 h-5 text-grey-700" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem
+                onSelect={() =>
+                  table.getColumn("category")?.setFilterValue(undefined)
+                }
+              >
+                All Transactions
+              </DropdownMenuItem>
+              {uniqueCategories.map((category) => (
+                <DropdownMenuItem
+                  key={category}
+                  onSelect={() =>
+                    table.getColumn("category")?.setFilterValue(category)
+                  }
+                >
+                  {category}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+
+      {/* Desktop filter/search bar */}
+      <div className="hidden md:flex items-center py-4 justify-between mb-4">
+        {/* Desktop Search */}
+        <Input
+          placeholder="Search recipient or sender..."
+          value={
+            (table.getColumn("recipient")?.getFilterValue() as string) ?? ""
+          }
+          onChange={(event) =>
+            table.getColumn("recipient")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+
+        {/* Desktop Filter & Sort */}
         <div className="flex items-center gap-4">
+          {/* Category Dropdown */}
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline">
@@ -102,9 +184,9 @@ export function DataTable<TData extends { category: string }, TValue>({
               {uniqueCategories.map((category) => (
                 <DropdownMenuItem
                   key={category}
-                  onSelect={() => {
-                    table.getColumn("category")?.setFilterValue(category);
-                  }}
+                  onSelect={() =>
+                    table.getColumn("category")?.setFilterValue(category)
+                  }
                 >
                   {category}
                 </DropdownMenuItem>
@@ -112,7 +194,7 @@ export function DataTable<TData extends { category: string }, TValue>({
             </DropdownMenuContent>
           </DropdownMenu>
 
-          {/* Sort Options */}
+          {/* Sort Dropdown */}
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline">
